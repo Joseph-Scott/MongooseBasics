c9e362fbd028dee47342ced7b01f33ea44d269b6
Joe Scott <jbs198585@gmail.com>
2022-04-13 06:43:10 -0400

    Validating Mongoode updates

--- product.js
+++ product.js
@@ -23,13 +23,33 @@ const productSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   },
-  categories: [String]
+  categories: [String],
+  qty: {
+    online: {
+      type: Number,
+      default: 0
+    },
+    inStore: {
+      type: Number,
+      default: 0
+    }
+  }
 })
 
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({ name: 'Bike Helmet', price: 19.50, categories: ['Cycling', 'Safety'] })
-bike.save()
+// const bike = new Product({ name: 'Tire Pump', price: 19.50, categories: ['Cycling'] })
+// bike.save()
+//   .then(data => {
+//     console.log("IT WORKED!")
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     console.log("OH NO ERROR!")
+//     console.log(err)
+//   })
+
+Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: -9 }, { new: true, runValidators: true })
   .then(data => {
     console.log("IT WORKED!")
     console.log(data);
